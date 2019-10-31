ae4382d7decc7ecacb594870ee221cf2b8898d0a
Olena <olena.pyvovar1234@gmail.com>
2019-10-31 13:57:06 +0200

    Create and connect server

---
+++ ATM_WCFService/ATMService.svc.cs
@@ -0,0 +1,11 @@
+﻿namespace ATM_WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ATMService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ATMService.svc or ATMService.svc.cs at the Solution Explorer and start debugging.
+    public class ATMService : IATMService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
---
+++ ATM_WCFService/IATMService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace ATM_WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IATMService" in both code and config file together.
+    [ServiceContract]
+    public interface IATMService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
