f805ab2c10792aa4b29fd0554ea36420c9b4bd89
Olena <olena.pyvovar1234@gmail.com>
2019-11-09 12:05:05 +0200

    Add wcf

---
+++ ATM_WCFService/IAtmService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ATM_WCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IAtmService" in both code and config file together.
+    [ServiceContract]
+    public interface IAtmService
+    {
+        [OperationContract]
+        void DoWork();
+
+
+    }
+}
