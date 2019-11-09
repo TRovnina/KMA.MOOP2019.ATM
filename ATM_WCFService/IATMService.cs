893c8bbcdbb633c7e2ec454f677f47e2f6ed3d20
Olena <olena.pyvovar1234@gmail.com>
2019-11-09 06:05:02 +0200

    Error server

--- ATM_WCFService/IATMService.cs
+++ ATM_WCFService/IATMService.cs
@@ -1,12 +1,30 @@
 ﻿using System.ServiceModel;
+using DBModels;
 
 namespace ATM_WCFService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IATMService" in both code and config file together.
     [ServiceContract]
     public interface IATMService
     {
         [OperationContract]
-        void DoWork();
+        ATM GetATMByCode(string atmCode);
+        [OperationContract]
+        Manager GetManagerById(string managerId);
+        [OperationContract]
+        Account GetAccountByNum(string accountNum);
+        [OperationContract]
+        bool AccountExist(string accountNum);
+        [OperationContract]
+        Client GetClientByItn(string clientItn);
+        [OperationContract]
+        void AddAction(DBModels.Action action);
+        [OperationContract]
+        void AddATMManagerAction(ATMManagerAction atmManagerAction);
+        [OperationContract]
+        void AddRegularPayment(RegularPayment regularPayment);
+        [OperationContract]
+        void SaveATM(ATM atm);
+        [OperationContract]
+        void SaveAccount(Account account);
     }
 }
---
+++ ClientTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+
+namespace ClientTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // ATMServiceReference.ATMServiceClient client = new ATMServiceClient();
+            
+        }
+    }
+}
